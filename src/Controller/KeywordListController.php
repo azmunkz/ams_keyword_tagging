62401fc29ea74ae7da8b0973a699e30ecc9efca8
Syah Noorazman <syah-noorazman_ishak@astro.com.my>
2025-06-13 11:00:45 +0800

    completed Phase 2

--- src/Controller/KeywordListController.php
+++ src/Controller/KeywordListController.php
@@ -1,1 +1,24 @@
 <?php
+
+namespace Drupal\ams_keyword_tagging\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Returns AMS Keyword listing page.
+ */
+class KeywordListController extends ControllerBase {
+
+  /**
+   * Displays a placeholder keyword list page.
+   */
+  public function list() {
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('AMS Keyword list will appear here.'),
+    ];
+  }
+
+}
